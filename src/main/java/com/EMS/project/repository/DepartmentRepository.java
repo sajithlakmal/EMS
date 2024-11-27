909128c84520de9513e880d9ac444f4670d458d7
Sajith <sajithandlakmal100@gmail.com>
2024-11-27 19:32:44 +0530

    DepartmentRepository added

---
+++ src/main/java/com/EMS/project/repository/DepartmentRepository.java
@@ -0,0 +1,14 @@
+package com.EMS.project.repository;
+
+import com.EMS.project.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+
+    Optional<Department> findByName(String name);
+
+}
