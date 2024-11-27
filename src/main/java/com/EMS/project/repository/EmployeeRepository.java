30b410baaad61788f17055eb18d1968e67e87074
Sajith <sajithandlakmal100@gmail.com>
2024-11-27 18:58:32 +0530

    EmployeeRepository added

--- src/main/java/com/EMS/project/repository/EmployeeRepository.java
+++ src/main/java/com/EMS/project/repository/EmployeeRepository.java
@@ -1,4 +1,15 @@
 package com.EMS.project.repository;
 
-public class EmployeeRepository {
+import com.EMS.project.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+    Optional<Employee> findByEmail(String email);
+
 }
